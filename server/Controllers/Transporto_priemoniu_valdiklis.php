a358b158d7a9c063b98808e0d6b79ef2801c3962
Dyzelis <blaivas1995@gmail.com>
2017-12-17 14:19:44 +0200

    Uzsakymo valdiklis
    pagrindines uzsakymo funkcijos
    (gali reikti papildyti asmenų (vairuotojų, darbuotojų) gavimu)

--- server/Controllers/Transporto_priemoniu_valdiklis.php
+++ server/Controllers/Transporto_priemoniu_valdiklis.php
@@ -43,36 +43,7 @@ class Transporto_priemoniu_valdiklis extends Controller {
         ', array());
         return $result;
     }
-
-   /* public function pasirinkti_TP(Request $req, Response $res){
-        $db = new Database();
-        $result = $db->query_String('
-        SELECT `transporto_priemone`.`Valstybinis_nr`, `transporto_priemone`.`Plotis`, 
-            `transporto_priemone`.`Aukstis`, `transporto_priemone`.`Modelis`,
-            `transporto_priemone`.`Galia`, `transporto_priemone`.`Variklio_darbinis_turis`,
-            `transporto_priemone`.`Rida`,
-            CONCAT(`transporto_priemone`.`Draudimas_galioja_nuo`, ' - ', `transporto_priemone`.`Draudimas_galioja_iki`) AS draudimas, 
-            CONCAT(`transporto_priemone`.`Apziura_galioja_nuo`, ' - ' , `transporto_priemone`.`Apziura_galioja_iki`) AS apziura,
-            `transporto_priemone`.`Pagaminimo_metai`, `transporto_priemone`.`Kuro_tipas`, 
-            `transporto_priemone`.`Didziausias_leidz_svoris`, `transporto_priemone`.`Spalva`,
-            `transporto_busena`.`Busena`, `transporto_busena`.`BusenaEN`, `vairavimo_kategorija`.`kategorija`,
-            `transporto_priemone`.`Svoris`, `transporto_priemone`.`Sedimu_vt_sk`
-        FROM `transporto_priemone`
-        INNER JOIN `transporto_busena` ON `transporto_priemone`.`Transporto_busena_Busenos_id` = `transporto_busena`.`Busenos_id`
-        INNER JOIN `vairavimo_kategorija` ON `vairavimo_kategorija`.`Kategorijos_id` = `transporto_priemone`.`Vairavimo_kategorija_Kategorijos_id`
-        WHERE `transporto_priemone`.`Valstybinis_nr` = ::nr
-        ', array(
-            "nr" => $req->body['Valstybinis_nr']
-        ));
     
-        if($result){
-            $res->send();
-        }
-        else{
-            $res->send(Response::BAD_REQUEST);
-        }
-    }*/
-
     //26 paveikslėlis 18
     public function issaugoti_busena(Request $req, Response $res){
         $db = new Database();
@@ -81,7 +52,7 @@ class Transporto_priemoniu_valdiklis extends Controller {
             SET `transporto_priemone`.`Transporto_busena_Busenos_id` = ::id
             WHERE `transporto_priemone`.`Valstybinis_nr` = ::numeris
          ', array(
-            "id" => $req->body['Transporto_busena_Busenos_id'],
+             "id" => $req->body['Transporto_busena_Busenos_id'],
             "numeris" => $req->body['numeris']
         ));
 
@@ -92,9 +63,11 @@ class Transporto_priemoniu_valdiklis extends Controller {
             $res->send(Response::BAD_REQUEST);
         }
     }
-    //auto indentifikavimo nr
-    //variklio nr
 
+    //galimi nauji atr:
+    // auto indentifikavimo nr
+    // variklio nr
+    
     public function issaugoti_nauja_TP(Request $req, Response $res){
         $db = new Database();
         $result = $db->query_String('
@@ -108,38 +81,38 @@ class Transporto_priemoniu_valdiklis extends Controller {
             `Didziausias_leidz_svoris`, `Spalva`,
             `Transporto_busena_Busenos_id`, `Vairavimo_kategorija_Kategorijos_id` 
             )
-        VALUES (
-            ::val_Nr, ::plotis, ::aukstis,
-            ::modelis, ::marke, ::galia, ::rida,
-            ::draudimo_pradzia, ::draudimo_pab,
-            ::apziuros_pradzia, ::apziuros_pab,
-            ::pagam_metai, ::kuro_tipas, :: svoris,
-            ::sed_vt_sk, ::variklio_turis,
-            ::max_svoris, ::spalva,
-            ::uzimtumas, ::kategorija            
-            )
-         ', array(
-            "val_Nr" => $req->body['valstyb_nr'],
-            "plotis" => $req->body['plotis'],
-            "aukstis" => $req->body['aukstis'],
-            "modelis" => $req->body['modelis'],
-            "marke" => $req->body['marke'],
-            "galia" => $req->body['galia'],
-            "rida" => $req->body['rida'],
-            "draudimo_pradzia" => $req->body['draudimas_nuo'],
-            "draudimo_pab" => $req->body['draduimas_iki'],
-            "apziuros_pradzia" => $req->body['apziura_nuo'],
-            "apziuros_pab" => $req->body['apziura_iki'],
-            "pagam_metai" => $req->body['pagam_metai'],
-            "kuro_tipas" => $req->body['kuro_tipas'],
-            "svoris" => $req->body['mase'],
-            "sed_vt_sk" => $req->body['sed_vt_sk'],
-            "max_svoris" => $req->body['max_mase'],
+            VALUES (
+                ::val_Nr, ::plotis, ::aukstis,
+                ::modelis, ::marke, ::galia, ::rida,
+                ::draudimo_pradzia, ::draudimo_pab,
+                ::apziuros_pradzia, ::apziuros_pab,
+                ::pagam_metai, ::kuro_tipas, :: svoris,
+                ::sed_vt_sk, ::variklio_turis,
+                ::max_svoris, ::spalva,
+                ::uzimtumas, ::kategorija            
+                )
+                ', array(
+                    "val_Nr" => $req->body['valstyb_nr'],
+                    "plotis" => $req->body['plotis'],
+                    "aukstis" => $req->body['aukstis'],
+                    "modelis" => $req->body['modelis'],
+                    "marke" => $req->body['marke'],
+                    "galia" => $req->body['galia'],
+                    "rida" => $req->body['rida'],
+                    "draudimo_pradzia" => $req->body['draudimas_nuo'],
+                    "draudimo_pab" => $req->body['draduimas_iki'],
+                    "apziuros_pradzia" => $req->body['apziura_nuo'],
+                    "apziuros_pab" => $req->body['apziura_iki'],
+                    "pagam_metai" => $req->body['pagam_metai'],
+                    "kuro_tipas" => $req->body['kuro_tipas'],
+                    "svoris" => $req->body['mase'],
+                    "sed_vt_sk" => $req->body['sed_vt_sk'],
+                    "max_svoris" => $req->body['max_mase'],
             "spalva" => $req->body['spalva'],
             "uzimtumas" => $req->body['uzimtumo_K'],
             "kategorija" => $req->body['v_kategorija']
         ));
-
+        
         if($result){
             $res->send();
         }
@@ -147,42 +120,42 @@ class Transporto_priemoniu_valdiklis extends Controller {
             $res->send(Response::BAD_REQUEST);
         }
     }
-
+    
     public function atnaujinti_TP_duom(Request $req, Response $res){
         $db = new Database();
         $result = $db->query_String('
         UPDATE `transporto_priemone`
         SET
-            `Plotis`, `Aukstis,
-            `Modelis`, `Marke`,
-            `Galia, `Rida`,
-            `Draudimas_galioja_nuo`,
-            `Draudimas_galioja_iki`,
-            `Apziura_galioja_nuo`,
-            `Apziura_galioja_iki`,
-            `Pagaminimo_metai`,
-            `Kuro_tipas`, `Svoris`,
-            `Sedimu_vt_sk`,
-            `Variklio_darbinis_turis`,
-            `Didziausias_leidz_svoris`,
-            `Spalva`,
-            `Vairavimo_kategorija_Kategorijos_id`
+        `Plotis`, `Aukstis,
+        `Modelis`, `Marke`,
+        `Galia, `Rida`,
+        `Draudimas_galioja_nuo`,
+        `Draudimas_galioja_iki`,
+        `Apziura_galioja_nuo`,
+        `Apziura_galioja_iki`,
+        `Pagaminimo_metai`,
+        `Kuro_tipas`, `Svoris`,
+        `Sedimu_vt_sk`,
+        `Variklio_darbinis_turis`,
+        `Didziausias_leidz_svoris`,
+        `Spalva`,
+        `Vairavimo_kategorija_Kategorijos_id`
         WHERE
-            `Plotis` = ::plotis, `Aukstis` = ::aukstis,
-            `Modelis` = ::modelis, `Marke` = ::marke,
-            `Galia` = ::galia, `Rida` = ::rida,
-            `Draudimas_galioja_nuo` = ::draudimo_pradzia,
-            `Draudimas_galioja_iki` = ::draudimo_pab,
-            `Apziura_galioja_nuo` = ::apziuros_pradzia,
-            `Apziura_galioja_iki` = ::apziuros_pab,
-            `Pagaminimo_metai` = ::pagam_metai,
-            `Kuro_tipas` = ::kuro_tipas, `Svoris` = :: svoris,
-            `Sedimu_vt_sk` = ::sed_vt_sk,
-            `Variklio_darbinis_turis` =::variklio_turis,
-            `Didziausias_leidz_svoris` = ::max_svoris,
-            `Spalva` = ::spalva,
-            `Vairavimo_kategorija_Kategorijos_id` = ::kategorija            
-         ', array(
+        `Plotis` = ::plotis, `Aukstis` = ::aukstis,
+        `Modelis` = ::modelis, `Marke` = ::marke,
+        `Galia` = ::galia, `Rida` = ::rida,
+        `Draudimas_galioja_nuo` = ::draudimo_pradzia,
+        `Draudimas_galioja_iki` = ::draudimo_pab,
+        `Apziura_galioja_nuo` = ::apziuros_pradzia,
+        `Apziura_galioja_iki` = ::apziuros_pab,
+        `Pagaminimo_metai` = ::pagam_metai,
+        `Kuro_tipas` = ::kuro_tipas, `Svoris` = :: svoris,
+        `Sedimu_vt_sk` = ::sed_vt_sk,
+        `Variklio_darbinis_turis` =::variklio_turis,
+        `Didziausias_leidz_svoris` = ::max_svoris,
+        `Spalva` = ::spalva,
+        `Vairavimo_kategorija_Kategorijos_id` = ::kategorija            
+        ', array(
             "plotis" => $req->body['plotis'],
             "aukstis" => $req->body['aukstis'],
             "modelis" => $req->body['modelis'],
@@ -201,7 +174,7 @@ class Transporto_priemoniu_valdiklis extends Controller {
             "spalva" => $req->body['spalva'],
             "kategorija" => $req->body['v_kategorija']
         ));
-
+        
         if($result){
             $res->send();
         }
@@ -209,16 +182,16 @@ class Transporto_priemoniu_valdiklis extends Controller {
             $res->send(Response::BAD_REQUEST);
         }
     }
-
+    
     public function salinti_TP(Request $req, Response $res){
         $db = new Database();
         $result = $db->query_String('
-            DELETE `transporto_priemone` 
-            WHERE `transporto_priemone`.`Valstybinis_nr` = ::numeris
-         ', array(
+        DELETE `transporto_priemone` 
+        WHERE `transporto_priemone`.`Valstybinis_nr` = ::numeris
+        ', array(
             "numeris" => $req->body['numeris']
         ));
-
+        
         if($result){
             $res->send();
         }
@@ -226,8 +199,34 @@ class Transporto_priemoniu_valdiklis extends Controller {
             $res->send(Response::BAD_REQUEST);
         }
     }
-
-
+    /* public function pasirinkti_TP(Request $req, Response $res){
+         $db = new Database();
+         $result = $db->query_String('
+         SELECT `transporto_priemone`.`Valstybinis_nr`, `transporto_priemone`.`Plotis`, 
+             `transporto_priemone`.`Aukstis`, `transporto_priemone`.`Modelis`,
+             `transporto_priemone`.`Galia`, `transporto_priemone`.`Variklio_darbinis_turis`,
+             `transporto_priemone`.`Rida`,
+             CONCAT(`transporto_priemone`.`Draudimas_galioja_nuo`, ' - ', `transporto_priemone`.`Draudimas_galioja_iki`) AS draudimas, 
+             CONCAT(`transporto_priemone`.`Apziura_galioja_nuo`, ' - ' , `transporto_priemone`.`Apziura_galioja_iki`) AS apziura,
+             `transporto_priemone`.`Pagaminimo_metai`, `transporto_priemone`.`Kuro_tipas`, 
+             `transporto_priemone`.`Didziausias_leidz_svoris`, `transporto_priemone`.`Spalva`,
+             `transporto_busena`.`Busena`, `transporto_busena`.`BusenaEN`, `vairavimo_kategorija`.`kategorija`,
+             `transporto_priemone`.`Svoris`, `transporto_priemone`.`Sedimu_vt_sk`
+         FROM `transporto_priemone`
+         INNER JOIN `transporto_busena` ON `transporto_priemone`.`Transporto_busena_Busenos_id` = `transporto_busena`.`Busenos_id`
+         INNER JOIN `vairavimo_kategorija` ON `vairavimo_kategorija`.`Kategorijos_id` = `transporto_priemone`.`Vairavimo_kategorija_Kategorijos_id`
+         WHERE `transporto_priemone`.`Valstybinis_nr` = ::nr
+         ', array(
+             "nr" => $req->body['Valstybinis_nr']
+         ));
+     
+         if($result){
+             $res->send();
+         }
+         else{
+             $res->send(Response::BAD_REQUEST);
+         }
+     }*/
 }
 
 ?>
--- server/Controllers/Uzsakymu_valdiklis.php
+++ server/Controllers/Uzsakymu_valdiklis.php
@@ -7,13 +7,173 @@ class Uzsakymu_valdiklis extends Controller {
         
     }
 
-
-    public function gauti_TP_sarasa() {
+    public function gauti_sarasa(Request $req, Response $res) {
         $db = new Database();
         $result = $db->query_String('
-        
+        SELECT 
+
+            /** Informacija apie užsakymą **/
+
+            `uzsakymas`.`Numeris`,
+            `uzsakymas`.`Uzsakymo_data`,
+            `uzsakymas`.`Atlikimo_data`,
+            `uzsakymas`.`Prioritetas`,
+            `uzsakymas`.`TransportoPriemone`,
+            
+            /** Būsena **/
+
+            `busena`.`Busenos_pavadinimas` AS busenaLT,
+            `busena`.`Busenos_pavEN` AS busenaEN,
+
+            /** išvykimo padalinys **/
+
+            `pIs`.`Inventorinis_numeris` AS pIs_ID,
+            `pIs`.`padalinio_pavadinimas` AS pIs_pavadinimas, 
+
+            /** atvykimo padalinys **/
+
+            `pI`.`Inventorinis_numeris` AS pI_ID,
+            `pI`.`padalinio_pavadinimas` AS pI_pavadinimas,
+
+            /** užsakymo sudarytojas ir vairuotojas **/
+
+            CONCAT(`a1`.`Vardas`, ' ',`a1`.`Pavarde`) AS sudare,
+            CONCAT(`a2`.`Vardas`, ' ',`a2`.`Pavarde`) AS vairuotojas
+        FROM `uzsakymas`
+            INNER JOIN `darbuotojas` ON `uzsakymas`.`Suformavo` = `darbuotojas`.`Tabelio_nr`
+            INNER JOIN `asmuo` a1 ON `darbuotojas`.`Asmuo_AsmensKodas` = `a1`.`AsmensKodas`
+            LEFT JOIN `vairuotojas` ON `uzsakymas`.`Vairuotojas` = `vairuotojas`.`Tabelio_nr`
+            LEFT JOIN `asmuo` a2 ON `vairuotojas`.`Asmuo_AsmensKodas` = `a2`.`AsmensKodas`
+            INNER JOIN `padalinys` pIs ON `uzsakymas`.`IsPadalinio` = `pIs`.`Inventorinis_numeris`
+            INNER JOIN `padalinys` pI ON `uzsakymas`.`IPadalini`= `pI`.`Inventorinis_numeris`
+            INNER JOIN `busena` ON `uzsakymas`.`Busena`= `busena`.`Busenos_id`
         ', array());
+        return $result;
     }
+
+    //26 paveikslėlis 18
+    public function keisti_prioriteta(Request $req, Response $res){
+        $db = new Database();
+        $result = $db->query_String('
+            UPDATE `uzsakymas` 
+            SET `uzsakymas`.`Prioritetas` = ::prior
+            WHERE `uzsakymas`.`Numeris` = ::numeris
+         ', array(
+            "prior" => $req->body['prioritetas'],
+            "numeris" => $req->body['numeris']
+        ));
+
+        if($result){
+            $res->send();
+        }
+        else{
+            $res->send(Response::BAD_REQUEST);
+        }
+    }
+    //auto indentifikavimo nr
+    //variklio nr
+
+    public function naujas_uzsakymas(Request $req, Response $res){
+        $db = new Database();
+        $result = $db->query_String('
+        INSERT INTO `uzsakymas` (
+            `Numeris`,
+            `Uzsakymo_data`,
+            `Prioritetas,
+            `Busena`,
+            `Suformavo`,
+            `Vairuotojas`,
+            `TransportoPriemone`,
+            `IsPadalinio,
+            `IPadalini`
+            )
+        VALUES (
+            ::nr, ::uzsakymo_data, ::prior,
+            ::busena, ::formuotojas, 
+            ::vairuotojas, ::tran_priemone,
+            ::is_padalinys, ::at_padalinys          
+            )
+         ', array(
+            "nr" => $req->body['nr'],
+            "uzsakymo_data" => $req->body['uzsakymo_data'],
+            "prior" => $req->body['prioritetas'],
+            "busena" => $req->body['busena'], // tiesiog įvesti 1
+            "formuotojas" => $req->body['formuotojas'],
+            "vairuotojas" => $req->body['vairuotojas'],
+            "tran_priemone" => $req->body['TP_valst_nr'],
+            "is_padalinys" => $req->body['is_padalinys'],
+            "at_padalinys" => $req->body['at_padalinys']
+        ));
+
+        if($result){
+            $res->send();
+        }
+        else{
+            $res->send(Response::BAD_REQUEST);
+        }
+    }
+
+    public function atnaujinti(Request $req, Response $res){
+        $db = new Database();
+        $result = $db->query_String('
+        UPDATE `uzsakymas`
+            SET
+            `Numeris`,
+            `Uzsakymo_data`,
+            `Prioritetas,
+            `Busena`,
+            `Suformavo`,
+            `Vairuotojas`,
+            `TransportoPriemone`,
+            `IsPadalinio,
+            `IPadalini`
+        WHERE
+            ::nr, ::uzsakymo_data, ::prior,
+            ::busena, ::formuotojas, 
+            ::vairuotojas, ::tran_priemone,
+            ::is_padalinys, ::at_padalinys         
+         ', array(
+            "nr" => $req->body['nr'],
+            "uzsakymo_data" => $req->body['uzsakymo_data'],
+            "prior" => $req->body['prioritetas'],
+            "busena" => $req->body['busena'],
+            "formuotojas" => $req->body['formuotojas'],
+            "vairuotojas" => $req->body['vairuotojas'],
+            "tran_priemone" => $req->body['TP_valst_nr'],
+            "is_padalinys" => $req->body['is_padalinys'],
+            "at_padalinys" => $req->body['at_padalinys']
+        ));
+
+        if($result){
+            $res->send();
+        }
+        else{
+            $res->send(Response::BAD_REQUEST);
+        }
+    }
+
+    //pasalinti = atsaukti?
+    public function salinti(Request $req, Response $res){
+        $db = new Database();
+        $result = $db->query_String('
+            DELETE `uzsakymas` 
+            WHERE `uzsakymas`.`Numeris` = ::numeris
+         ', array(
+            "numeris" => $req->body['numeris']
+        ));
+
+        if($result){
+            $res->send();
+        }
+        else{
+            $res->send(Response::BAD_REQUEST);
+        }
+    }
+
+    //asmenu pasirinkimas
+    /*public function asmenys(Request $req, Response $res) {
+
+    }*/
 }
 
 ?>
